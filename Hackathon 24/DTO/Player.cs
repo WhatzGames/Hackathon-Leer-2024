ed7aaf0f98bdb6cd62bbb0d05155907f9d6f66de
Kurt <k.sap@journaway.com>
2024-09-27 11:14:21 +0200

    added dtos

---
+++ Hackathon 24/DTO/Player.cs
@@ -0,0 +1,7 @@
+﻿namespace Hackathon_24.DTO;
+
+public class Player
+{
+    public string? id { get; set; }
+    public int? score { get; set; }
+}
---
+++ Hackathon 24/DTO/Round.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Emit;
+using System.Reflection.PortableExecutable;
+using System.Security.AccessControl;
+
+namespace Hackathon_24.DTO;
+
+public class Round
+{
+    public string? id { get; set; }
+    public List<Player>? players { get; set; }
+    public string? word { get; set; }
+    public List<char>? guessed { get; set; }
+    public List<LogEntry>? log { get; set; }
+    public string? type { get; set; }
+    public string? self { get; set; }
+}
