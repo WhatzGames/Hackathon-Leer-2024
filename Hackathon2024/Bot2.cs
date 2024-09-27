dfc83a7d0730b60da71ddc42c12a4ac195e3ebf4
okooijman <o.kooijman@journaway.com>
2024-09-27 15:20:27 +0200

    Added Bot2

---
+++ Hackathon2024/Bot2.cs
@@ -0,0 +1,36 @@
+﻿using Hackathon_24;
+using Hackathon2024.DTO;
+
+namespace Hackathon2024 ;
+
+    public class Bot2 : IBot
+    {
+        private List<char> _probabilities =
+            LettersToPercentage.GetLettersToPercentage()
+                               .OrderBy(x => x.Value)
+                               .Select(x => x.Key)
+                               .ToList();
+
+        private int counter = -1;
+        public string BotName { get; }
+
+        public Bot2(string botName)
+        {
+            BotName = botName;
+        }
+
+        public char CalculateNextStep(Round round)
+        {
+            counter++;
+            if (counter == _probabilities.Count)
+            {
+                return '*';
+            }
+            return _probabilities[counter];
+        }
+
+        public int Complete(Result result)
+        {
+            return 0;
+        }
+    }
