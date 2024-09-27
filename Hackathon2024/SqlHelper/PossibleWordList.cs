5fd15d5c2c4ed4b0ddb06df61582b8ec0453169b
Kurt <k.sap@journaway.com>
2024-09-27 15:57:46 +0200

    refac

---
+++ Hackathon2024/SqlHelper/PossibleWordList.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Hackathon2024.SqlHelper;
+
+public class PossibleWordList(DbConfiguration dbConfiguration)
+{
+    private readonly List<string> _possibleWordList = [];
+
+    public List<string> GetPossibleWordList(string word)
+    {
+        var sql = BuildSql(word);
+        try
+        {
+            using var connection = dbConfiguration.GetDatabaseConnection();
+
+            using var command = new SqliteCommand(sql, connection);
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    _possibleWordList.Add(reader.GetString(1));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No entrys found in database for " + word);
+            }
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        
+        return _possibleWordList;
+    }
+    
+    private string BuildSql(string word)
+    {
+        return "SELECT * FROM " + dbConfiguration.GetTableName() + " WHERE "
+             + dbConfiguration.GetColumnName() + "LIKE \'" + word + "\'";
+    }
+}
